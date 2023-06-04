3a0fc8ffeb8b922a238157d610dd86abe24364b7
mfrancese <francese.mariano@gmail.com>
2023-06-04 19:44:00 -0300

    Linea comentada

--- SolucionCAI.AgenciaDeViajes/Archivos/ModuloProductos.cs
+++ SolucionCAI.AgenciaDeViajes/Archivos/ModuloProductos.cs
@@ -13,41 +13,41 @@ namespace SolucionCAI.AgenciaDeViajes.Archivos
 {
     public class ModuloProductos
     {
-        public static List<VueloEnt> ListaVuelos(string origen, string destino, DateTime fechaPartida, int cantPasajeros, string tipoPasajero, string clase)
-        {
+        //public static List<VueloEnt> ListaVuelos(string origen, string destino, DateTime fechaPartida, int cantPasajeros, string tipoPasajero, string clase)
+        //{
             
 
-            if (File.Exists("vuelos.json"))
-            {
-                string contenidoDelArchivo = File.ReadAllText("vuelos.json");
+        //    if (File.Exists("vuelos.json"))
+        //    {
+        //        string contenidoDelArchivo = File.ReadAllText("vuelos.json");
 
-                JObject jsonObject = JObject.Parse(contenidoDelArchivo);
+        //        JObject jsonObject = JObject.Parse(contenidoDelArchivo);
 
-                //List<dynamic> vuelosJson = JsonConvert.DeserializeObject<List<dynamic>>(contenidoDelArchivo);          
-                List<VueloEnt> vuelosFiltrados = new List<VueloEnt>();
+        //        //List<dynamic> vuelosJson = JsonConvert.DeserializeObject<List<dynamic>>(contenidoDelArchivo);          
+        //        List<VueloEnt> vuelosFiltrados = new List<VueloEnt>();
 
-                foreach (var contenido in contenidoDelArchivo)
-                {
-                    string origenjson = (string)jsonObject["origen"];
-                    string destinojson = (string)jsonObject["destino"];
-                    string fechapartidajson = (string)jsonObject["fecha de partida"];
-                    string cantpjson = (string)jsonObject["cantidad pasajeros"];
-                    string tipopjson = (string)jsonObject["tipo de pasajero"];
-                    string clasejson = (string)jsonObject["clase"];
+        //        foreach (var contenido in contenidoDelArchivo)
+        //        {
+        //            string origenjson = (string)jsonObject["origen"];
+        //            string destinojson = (string)jsonObject["destino"];
+        //            string fechapartidajson = (string)jsonObject["fecha de partida"];
+        //            string cantpjson = (string)jsonObject["cantidad pasajeros"];
+        //            string tipopjson = (string)jsonObject["tipo de pasajero"];
+        //            string clasejson = (string)jsonObject["clase"];
 
-                    if (origenjson==origen && destinojson==destino && DateTime.Parse(fechapartidajson)==fechaPartida && Int64.Parse(cantpjson)==cantPasajeros && tipopjson==tipoPasajero && clasejson==clase)
-                    {
+        //            if (origenjson==origen && destinojson==destino && DateTime.Parse(fechapartidajson)==fechaPartida && Int64.Parse(cantpjson)==cantPasajeros && tipopjson==tipoPasajero && clasejson==clase)
+        //            {
 
                         
-                    }
+        //            }
 
-                }
-            }
-            else
-            {
-                jsonVuelos = new List<VueloEnt>();
-                return jsonVuelos;
-            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        jsonVuelos = new List<VueloEnt>();
+        //        return jsonVuelos;
+        //    }
         }
         
     }
