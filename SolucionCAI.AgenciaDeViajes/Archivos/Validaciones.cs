ab151e2564a75dc301b8f876c16d2969a4228a3e
Antonella Orlando <anto_orlando@outlook.com>
2023-06-05 19:35:51 -0300

    Cambio en Validaciones, tanto esta como la anterior fueron en Validaciones!

--- SolucionCAI.AgenciaDeViajes/Archivos/Validaciones.cs
+++ SolucionCAI.AgenciaDeViajes/Archivos/Validaciones.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using SolucionCAI.AgenciaDeViajes.Entidades;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SolucionCAI.AgenciaDeViajes.Archivos
 {
@@ -28,11 +31,11 @@ namespace SolucionCAI.AgenciaDeViajes.Archivos
         {
             bool flag = false;
 
-            if (String.IsNullOrWhiteSpace(usuario) || String.IsNullOrWhiteSpace(contraseña))
+            if (string.IsNullOrWhiteSpace(usuario) || string.IsNullOrWhiteSpace(contraseña))
             {
                 Console.WriteLine("Usuario/Contraseña no puede estar vacio.");
             }
-            
+
             else
             {
                 flag = true;
@@ -62,37 +65,60 @@ namespace SolucionCAI.AgenciaDeViajes.Archivos
                 return true;
             }
         }
+
+
+
+        public bool ValidaDNI(int dni)
+        {
+            const int longitudDNI = 8;
+
+            string dniString = dni.ToString();
+
+            // Verificar la longitud del DNI
+            if (dniString.Length != longitudDNI)
+            {
+                Console.WriteLine("El DNI ingresado no es válido.");
+                return false;
+            }
+
+            // Verificar que el DNI esté compuesto solo por dígitos
+            foreach (char c in dniString)
+            {
+                if (!Char.IsDigit(c))
+                {
+                    Console.WriteLine("El DNI ingresado no es válido.");
+                    return false;
+                }
+            }
+
+            // Si todas las validaciones son exitosas, se asume que el DNI es válido
+            return true;
+        }
+
+
+        /* Para llamar a esta validación
+          PasajeroEnt pasajero = new PasajeroEnt();
+            pasajero.DN = 12345678;
+
+            bool dniValido = ValidaDNI(pasajero.DN);
+                if (dniValido)
+                {
+                Console.WriteLine("El DNI es válido.");
+                            }
+                        else
+                {
+                    Console.WriteLine("El DNI no es válido.");
+                }*/
+
+
+
+
+
+
+
+
+
     }
 
-
-
-        public bool ValidaDni(int DNI)
-            {
-                bool flag = false;
-
-                if (!int.TryParse(numero, out salida))
-                     {
-                         Console.WriteLine("Usted debe ingresar un dato numérico.");
-                       }
-                 else if (salida <= 0)
-                      {
-                        Console.WriteLine("Usted debe ingresar un número mayor que cero.");
-                         }
-                  else
-                        {
-                            flag = true;
-                        }
-
-                     return flag;
-               }
-
-
-
-
-
-
-
 }
-
-
 
